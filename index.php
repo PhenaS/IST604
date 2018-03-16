1477ab4aca8c90adcf9d7cab46551fb84ad9d584
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-16 12:21:02 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -74,8 +74,8 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
 
    <!-- Subscribe section -->
   <div class="w3-container w3-black w3-padding-32">
-    <h1>Subscribe</h1>
-    <p>To get special offers and VIP treatment:</p>
+    <h1>Interested in Working With us?</h1>
+    <p>Enter your email and we will contact you:</p>
     <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail" style="width:100%"></p>
     <button type="button" class="w3-button w3-red w3-margin-bottom">Subscribe</button>
   </div>
