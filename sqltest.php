065fb28595b5eb0eec058b9dbe4d2b75ce00cd61
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-17 12:24:08 -0400

    Add files via upload

--- sqltest.php
+++ sqltest.php
@@ -1,13 +1,14 @@
-<?php
-$serverName = "phena.database.windows.net";
-$connectionOptions = array(
-    "Database" => "IST604",
-    "Uid" => "senediak",
-    "PWD" => "ste11PHEN**"
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-$tsql= "SELECT * FROM employee";
-$getResults= sqlsrv_query($conn, $tsql);
-echo $getResults;
-?>
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:phena.database.windows.net,1433; Database = IST604", "senediak", "ste11PHEN**");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "senediak@phena", "pwd" => "ste11PHEN**", "Database" => "IST604", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:phena.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
