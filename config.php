1978851d9a132fcc751f098f3c68548e317205dc
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-17 10:19:21 -0400

    Add files via upload

--- config.php
+++ config.php
@@ -7,10 +7,10 @@ define('DB_PASSWORD', 'ste11PHEN**');
 define('DB_NAME', 'IST604');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = sqlsrv_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . sqlsrv_connect_error());
 }
 ?>
