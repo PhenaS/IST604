03c795ee31fad15b60d945cdae373f4f7ee88c75
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-16 21:22:25 -0400

    Add files via upload

--- locations.php
+++ locations.php
@@ -8,27 +8,16 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-* {
-    box-sizing: border-box;
-}
+.w3-sidebar a {font-family: "Roboto", sans-serif}
 body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
 
-.row {
-    display: flex;
-}
-
-/* Create two equal columns that sits next to each other */
 .column {
     flex: 50%;
     padding: 10px;
     height: 300px; /* Should be removed. Only for demonstration */
 }
 </style>
-</head>
-<body>
-
-<h2>Two Equal Columns</h2>
-
+<body class="w3-content" style="max-width:1200px">
 <div class="row">
   <div class="column" style="background-color:#aaa;">
     <h2>Column 1</h2>
@@ -38,10 +27,51 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     <h2>Column 2</h2>
     <p>Some text..</p>
   </div>
+    <div class="column" style="background-color:#aaa;">
+    <h2>Column 3</h2>
+    <p>Some text..</p>
+  </div>
+  <div class="column" style="background-color:#bbb;">
+    <h2>Column 4</h2>
+    <p>Some text..</p>
+  </div>
 </div>
-
 </body>
 </html>
+        
+
+<!-- Top menu on small screens -->
+<<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+  <div class="w3-bar-item w3-padding-24 w3-wide">SAW</div>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+ </header>
+
+  <!-- Top header -->
+  <header class="w3-container w3-xlarge">
+    <p class="w3-left">Solutions At Work Inc</p>
+    <p class="w3-right">
+      <i class="fa fa-search"></i>
+    </p>
+  </header>
+
+<div class="w3-main" style="margin-left:10px">
+    <body>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2987.836252065478!2d-81.69073538457049!3d41.50782317925326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8830fa78535c1473%3A0x7e7677594d50f8c3!2s1275+Lakeside+Ave+E%2C+Cleveland%2C+OH+44114!5e0!3m2!1sen!2sus!4v1521238460812" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <p><b>  Main Office</b></p>
+        <p><i class="fa fa-fw fa-map-marker"></i>1275 Lakeside Ave, Cleveland, OH 44114</p> 
+        <p><i class="fa fa-fw fa-phone"></i> 012-345-6789</p>
+        <p><i class="fa fa-fw fa-envelope"></i> ex@mail.com</p>
+        <br><br>
+    </body>
+</div>
+<div class="w3-main" style="margin-left:800px">
+    <body>
+        <p><b> Beachwood Adult Activity Center</b></p>
+        <p><i class="fa fa-fw fa-map-marker"></i></p> 
+        <p><i class="fa fa-fw fa-phone"></i> 012-345-6789</p>
+        <p><i class="fa fa-fw fa-envelope"></i> ex@mail.com</p>           
+    </body>
+</div>
 
   
 
