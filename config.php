ed5358b3e4e40828a9caaa1905472c8ecc313ca0
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-17 10:22:14 -0400

    Add files via upload

--- config.php
+++ config.php
@@ -11,6 +11,6 @@ $link = sqlsrv_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . sqlsrv_connect_error());
+    die("ERROR: Could not connect. " . sqlsrv_error());
 }
 ?>
