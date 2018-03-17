7b173ba9a291ac0c5ae4de7233763326ef0e6807
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-16 21:02:08 -0400

    Add files via upload

--- locations.php
+++ locations.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html>
-<title>Solutions At Work Inc.</title>
-<meta charset="UTF-8">
+<head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-.w3-sidebar a {font-family: "Roboto", sans-serif}
-body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+* {
+    box-sizing: border-box;
+}
 
+.row {
+    display: flex;
+}
+
+/* Create two equal columns that sits next to each other */
 .column {
     flex: 50%;
     padding: 10px;
     height: 300px; /* Should be removed. Only for demonstration */
 }
 </style>
-<body class="w3-content" style="max-width:1200px">
+</head>
+<body>
+
+<h2>Two Equal Columns</h2>
+
 <div class="row">
   <div class="column" style="background-color:#aaa;">
     <h2>Column 1</h2>
@@ -27,18 +32,10 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     <h2>Column 2</h2>
     <p>Some text..</p>
   </div>
-    <div class="column" style="background-color:#aaa;">
-    <h2>Column 3</h2>
-    <p>Some text..</p>
-  </div>
-  <div class="column" style="background-color:#bbb;">
-    <h2>Column 4</h2>
-    <p>Some text..</p>
-  </div>
 </div>
+
 </body>
 </html>
-        
 
   
 
