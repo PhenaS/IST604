463c05edf7cae61efdb11b0b74beaad2beff794a
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 12:56:39 -0400

    Add files via upload

--- sql.php
+++ sql.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "phena.database.windows.net";
+$serverName = "tcp:phena.database.windows.net,1433;Initial";
 $connectionOptions = array(
     "Database" => "phena",
     "Uid" => "senediak",
@@ -10,9 +10,12 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
 $tsql= "Select first_name, emp_id FROM employee";
 $getResults= sqlsrv_query($conn, $tsql);
 echo ("Reading data from table" . PHP_EOL);
-echo $getrsults
+
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo ($row['first_name'] . " " . $row['emp_id'] . PHP_EOL);
+}
 sqlsrv_free_stmt($getResults);
 ?>
