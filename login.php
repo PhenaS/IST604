83ad6b49cee454ddf5a88f8b491bfb1aa5013c3c
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 13:18:30 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
-<title>Solutions At Work Inc.</title>
-<meta charset="UTF-8">
+    <head>
+        <title>Solutions At Work Inc.</title>
+        <?php include "db/connect.php"?>
+    </head>
+    <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
