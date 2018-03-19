a7a4c1eba88d143303274b29cf16bb3a287c528c
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 19:19:08 -0400

    Add files via upload

--- login2.php
+++ login2.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
 <html>
     <body>
-        <?php
-echo "$row[0] $row[1]";
+    try{
+    $host = "tcp:phena.database.windows.net,1433";
+    $user = "senediak";
+    $pwd = "ste11PHEN**";
+    $db = "IST604";
+    $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = ("SELECT first_name, last_name FROM employee WHERE emp_id='{$_POST["EmployeeID"]}'"); 
+    $stmt = $conn->query("$sql");
+    $row = $stmt->fetch();
+    echo "$row[0] $row[1]";
+    $conn = NULL;
+    }catch(Exception $e){die(print_r($e));}
     ?>
     </body>
 </html>
