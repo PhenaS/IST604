5f376cfa654668b5912725365bc95ee2bcb7dacb
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 13:17:05 -0400

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+
+//local server info
+$server = "tcp:phena.database.windows.net,1433";
+$username = senediak;
+$ste11PHEN**
+$db = "phena"
+
+//check connection
+
+try {
+    $handle = PDO("sqlsrv:host=$server;dbname=$db","$username", "$password");
+    $handle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected";
+} catch(PDOException $e) {
+  die("Oops. Something went wrong in the database.")
+  $e->getMessage();
+}
+?>
