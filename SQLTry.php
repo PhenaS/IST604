63df56c7da3950fbb720ff3f622628cdf6891ad5
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 18:38:23 -0400

    Add files via upload

--- SQLTry.php
+++ SQLTry.php
@@ -1,23 +1,15 @@
 <?php 
-
-$host = "tcp:phena.database.windows.net,1433";
-$user = "senediak";
-$pwd = "ste11PHEN**";
-$db = "IST604";
-$conn = new PDO ("sqlsrv:Server = $host; Datbase = $db", $user, $pwd);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = ("SELECT first_name, last_name FROM employee WHERE emp_id = 100"); 
-$stmt = $conn->query("sql");
-$row = $stmt->fetch();
-echo "$row[0] $row[1] $row[2]";
-$conn = NULL;
-
-if(sqlsrv_has_rows($stmt)) 
-{ 
-    echo "Welcome."; 
-} 
-else 
-{ 
-    echo "Invalid password."; 
-} 
+try{
+    $host = "tcp:phena.database.windows.net,1433";
+    $user = "senediak";
+    $pwd = "ste11PHEN**";
+    $db = "IST604";
+    $conn = new PDO ("sqlsrv:Server = $host; Datbase = $db", $user, $pwd);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = ("SELECT first_name, last_name FROM employee WHERE emp_id = 100"); 
+    $stmt = $conn->query("sql");
+    $row = $stmt->fetch();
+    echo "$row[0] $row[1] $row[2]";
+    $conn = NULL;
+}catch(Exception $e ){die(print_r($e));}
 ?>
