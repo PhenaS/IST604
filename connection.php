86a709c69eaded03541bb697ad434e754b96cb8c
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 14:08:02 -0400

    Add files via upload

--- connection.php
+++ connection.php
@@ -12,7 +12,7 @@ try {
     $db = "phena";
     $conn = new PDO("sqlsrv:Server = $host; Database = $db","$user", "$pwd");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = ("SELECT * FROM employee WHERE emp_id" = 100);
+    $sql = ("SELECT * FROM employee WHERE emp_id = 100");
     $stmt = $conn->query("$sql");
     $row = $stmt->fetch();
     echo "$row[0] $row[1] $row[2]";
