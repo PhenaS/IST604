6dd85e01c0ee908dcb3d54606ca3a1f3eda9eb97
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 17:07:45 -0400

    Add files via upload

--- Action_page.php
+++ Action_page.php
@@ -4,6 +4,7 @@ try {
     $conn = new PDO("sqlsrv:Server = $host;Database = $db","$user","$pwd");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = ("SELECT first_name, last_name FROM employee WHERE emp_id = '{$_POST[EmployeeID]}'");
+    $sql=sqlsrv_query(sprintf("SELECT first_name, last_name FROM employee WHERE emp_id='{$_POST[EmployeeID]}'",sqlsrv_escape_string($_POST['user'])));
     $stmt = $conn->query("$sql");
     $emp = $stmt->fetch();
     echo "$emp[0] $emp[1] $emp[2]";
