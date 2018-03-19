53bc97d0706435c761c2be807e2db6fd152318ee
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 14:52:39 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,16 +1,10 @@
 <?php
-
-//local server info
-
-
-//check connection
-
 try {
-    $host = "phena.database.windows.net";
+    $host = "tcp:phena.database.windows.net,1433";
     $user = "senediak";
     $pwd = "ste11PHEN**";
     $db = "phena";
-    $conn = new PDO("sqlsrv:Server = $host; Database = $db","$user", "$pwd");
+    $conn = new PDO("sqlsrv:Server = $host;Database = $db","$user", "$pwd");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = ("SELECT * FROM employee WHERE emp_id = 100");
     $stmt = $conn->query("$sql");
