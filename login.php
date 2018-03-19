19b34a678de9b8d2959211bb6a8e702c60bc1491
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 15:05:00 -0400

    Update login.php

--- login.php
+++ login.php
@@ -20,10 +20,14 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
       <div class="w3-col s4">
         <h4>Solutions at Work Inc</h4>
         <p>SAW Staff Login</p>
-        <form action="/action_page.php" target="_blank">
+        <form action="/action_page.php" method="post">
           <p><input class="w3-input w3-border" type="text" placeholder="EmployeeID" name="EmployeeID" required></p>
           <p><input class="w3-input w3-border" type="text" placeholder="PIN" name=PIN" required></p>
           <button type="submit" class="w3-button w3-block w3-black">Submit</button>
         </form> 
       </div>
-</body>
+ </body>
+ <?php
+ echo _POST["EmployeeID"];
+ echo_POST["PIN"};
+           ?>
