a1a10be802edc07b71ee5d6e546eef92258251a1
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-19 19:07:59 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
   <div class="w3-col s4">
     <h4>Solutions at Work Inc</h4>
     <p>SAW Staff Login</p>
-      <form action="/action_page.php" method="post">
+      <form id="login" action = "login.php" method="post">
         <p><input class="w3-input w3-border" type="text" placeholder="EmployeeID" name="EmployeeID" required></p>
         <p><input class="w3-input w3-border" type="password" placeholder="Password" name="Password" required></p>
         <button type="submit" class="w3-button w3-block w3-black">Submit</button>
