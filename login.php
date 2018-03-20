acfd76929d4ee524429f29306e676983dff05a24
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-20 16:03:39 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -41,6 +41,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     }
     elseif($_POST["Password"] !== $row[0]){
     echo "Incorrect Username or Password";
+    }
 ?>   
   </div> 
 </body>
