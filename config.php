b0e43d250193c5223ae66500419e83700204f4f8
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-20 16:32:20 -0400

    Add files via upload

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php 
-try{
     $host = "tcp:phena.database.windows.net,1433";
     $user = "senediak";
     $pwd = "ste11PHEN**";
     $db = "IST604";
-    $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(Exception $e){die(print_r($e));
 ?>
--- login.php
+++ login.php
@@ -27,10 +27,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
      
 <?php 
 try{
-    $host = "tcp:phena.database.windows.net,1433";
-    $user = "senediak";
-    $pwd = "ste11PHEN**";
-    $db = "IST604";
+    require('congif.php')
     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = ("SELECT pwd, first_name, last_name FROM employee WHERE emp_id='{$_POST["EmployeeID"]}'"); 
