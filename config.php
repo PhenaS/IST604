bb6b2ee3f5d0cb63b78880ec33683bff49af1dc8
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-20 15:57:50 -0400

    Add files via upload

--- config.php
+++ config.php
@@ -1,6 +1,10 @@
-<?php    
+<?php 
+try{
     $host = "tcp:phena.database.windows.net,1433";
     $user = "senediak";
     $pwd = "ste11PHEN**";
     $db = "IST604";
+    $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(Exception $e){die(print_r($e));
 ?>
