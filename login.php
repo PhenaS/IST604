40072edf13cc37ffe3f9751aa6d276bd4b0cb21f
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-20 16:23:47 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     $row = $stmt->fetch();
     $conn = NULL;
 
-    echo $row[1];
+    header('Location: dashboard.php');
 ?>   
 </body>
 </html>
