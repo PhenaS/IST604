5ac844be76a3269c59e0f675cdcd515704274aed
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-21 16:22:09 -0400

    Update checkauth.php

--- checkauth.php
+++ checkauth.php
@@ -1,16 +1,33 @@
-<?php
-include 'login.php';
+<?php 
+     
+$username = null;
+$password = null;
 
-if($emppwd == $row[0])
-{
-        session_start();
-        $_SESSION['sid']=session_id();
-        echo $row[0];
-        header("location:dashboard.php");
-}
-else
-{
-header("location:login.php");
-}
+try{
+    include 'config.php';
+    
+    $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = ("SELECT pwd, emp_id first_name, last_name FROM employee WHERE emp_id='$empid_POST["employeeid"]'"); 
+    $stmt = $conn->query("$sql");
+    $row = $stmt->fetch();
+    $conn = NULL;
+    }catch(Exception $e){die(print_r($e));}
 
-?>
+
+
+if(!empty($_POST["employeeid"]) && !empty($_POST["password"])) {
+        $empid = $_POST["employeeid"];
+        $password = $_POST["password"];
+else{
+            header('Location: login.php');
+     }
+   
+if($username == $row[1] && $password == row[0]) {
+  session_start();
+  $_SESSION["authenticated"] = 'true';
+  header('Location: dashboard.php');
+}else {
+        header('Location: login.php');
+       }
+?>    
