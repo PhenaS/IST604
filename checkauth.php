62a6141150fd32af70c3c8c538dfe6caa8132eea
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-21 20:42:35 -0400

    Add files via upload

--- checkauth.php
+++ checkauth.php
@@ -5,12 +5,11 @@ try{
     
     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = ("SELECT pwd, emp_id first_name, last_name FROM employee WHERE emp_id='$empid_POST["employeeid"]'"); 
+    $sql = ("SELECT pwd, first_name, last_name FROM employee WHERE emp_id= '" . $_POST['employeeid'] . "'"); 
     $stmt = $conn->query("$sql");
     $row = $stmt->fetch();
     $conn = NULL;
     }catch(Exception $e){die(print_r($e));}
-
 
 
 if(!empty($_POST["employeeid"]) && !empty($_POST["password"])) {
