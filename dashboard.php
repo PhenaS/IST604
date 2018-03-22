59ae280bc8a6334db438b8be02eb1fcbc8530ec6
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-21 21:27:29 -0400

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html>
 <title>Solutions At Work Inc.</title>
-<?php
-if ($_SESSION['authorized'] == false) {
-  header ("Location: login.php");
-}
-?>
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
