9aac48130854d498219f8b11a82399241ae30fe0
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-21 22:01:59 -0400

    Add files via upload

---
+++ checkauthind.php
@@ -0,0 +1,29 @@
+<?php 
+   
+try{
+    include 'config.php';
+    
+    $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = ("SELECT username, pin FROM employee WHERE username= '" . $_POST["Username"] . "'"); 
+    $stmt = $conn->query("$sql");
+    $row = $stmt->fetch();
+    $conn = NULL;
+    }catch(Exception $e){die(print_r($e));}
+
+
+if(!empty($_POST["Username"]) && !empty($_POST["PIN"])) {
+        $indun = $_POST["Username"];
+        $pin = $_POST["PIN"];
+}else{
+            header('Location: login.php');
+     }
+   
+if("$password" == "$row[0]") {
+  session_start();
+  $_SESSION["authenticated"] = 'true';
+  header('Location: dashboard.php');
+}else {
+  header('Location: login.php');
+       }
+?>
