ea19eca68b7e3e178b742dda26ac914858da1399
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-24 11:36:26 -0400

    Add files via upload

---
+++ paytest.php
@@ -0,0 +1,20 @@
+    <?php 
+   
+try{
+    include 'config.php';
+    
+    $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = ("SELECT username, pin, first_name FROM individual WHERE username = '" . $_POST["Username"] . "'"); 
+    $stmt = $conn->query("$sql");
+    $sow = $stmt->fetch();
+    $conn = NULL;
+    }catch(Exception $e){die(print_r($e));}
+    
+    
+    echo $sow[2];
+       
+    
+?>
+
+
