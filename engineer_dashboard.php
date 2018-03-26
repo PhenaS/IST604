fe5f839928d494e63416d454f1d312e00f4a41e8
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-26 16:21:33 -0400

    Update engineer_dashboard.php

--- engineer_dashboard.php
+++ engineer_dashboard.php
@@ -1,51 +1,26 @@
 <!DOCTYPE html>
 <html>
-
-<title>Engineer Dashboard</title>
+<title>W3.CSS</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-
-p.speech {
-  position: relative;
-  width: 80000px;
-  height: 40px;
-  text-align: center;
-  line-height: 100px;
-  background-color: #fff;
-  border: 8px solid #666;
-  -webkit-border-radius: 30px;
-  -moz-border-radius: 30px;
-  border-radius: 30px;
-  -webkit-box-shadow: 2px 2px 4px #888;
-  -moz-box-shadow: 2px 2px 4px #888;
-  box-shadow: 2px 2px 4px #888;
-}  
-  </style>
-<body class="w3-content" style="max-width:1200px"
+<body>
 
 <div class="w3-sidebar w3-bar-block w3-dark-grey w3-animate-left" style="display:none" id="mySidebar">
   <button class="w3-bar-item w3-button w3-large"
   onclick="w3_close()">Close &times;</button>
-  <a href="#" class="w3-bar-item w3-button">Ratings Due Dates</a>
-  <a href="#" class="w3-bar-item w3-button">Rating Tool</a>
-  <a href="#" class="w3-bar-item w3-button">Analog Rating Entry</a>
-  <a href="#" class="w3-bar-item w3-button">View Current Rates</a>
-  <a href="#" class="w3-bar-item w3-button">View Historial Rates</a>
+  <a href="#" class="w3-bar-item w3-button">Link 1</a>
+  <a href="#" class="w3-bar-item w3-button">Link 2</a>
+  <a href="#" class="w3-bar-item w3-button">Link 3</a>
 </div>
 
 <div>
   <button class="w3-button w3-white w3-xxlarge" onclick="w3_open()">&#9776;</button>
   <div class="w3-container">
-    <h1>Engineer Dashboard</h1>
-    <p class="speech">New job arriving at Euclid AAC Today</p>
-      </div>
+    <h1>Animated Sidebar</h1>
+    <p>Click on the "hamburger menu" to slide in the side navigation.</p>
+    <p>W3.CSS provide the following animation classes if you want to experiment for yourself:</p>
+    <p>w3-animate-left, w3-animate-top, w3-animate-bottom, w3-animate-right, w3-animate-opacity, w3-animate-zoom</p>
+  </div>
 </div>
 
 <script>
