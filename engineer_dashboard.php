5623e14e7ae0fed95bf0e9bceea302eb7dc320d5
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-26 16:09:47 -0400

    Update engineer_dashboard.php

--- engineer_dashboard.php
+++ engineer_dashboard.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <title>Engineer Dashboard</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+
 p.speech {
   position: relative;
   width: 1200px;
@@ -20,7 +30,7 @@ p.speech {
   box-shadow: 2px 2px 4px #888;
 }  
   </style>
-<body>
+<body class="w3-content" style="max-width:1200px"
 
 <div class="w3-sidebar w3-bar-block w3-dark-grey w3-animate-left" style="display:none" id="mySidebar">
   <button class="w3-bar-item w3-button w3-large"
