c52516667a1d1eed7531caeab0f820388909e23d
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-03-30 15:05:42 -0400

    Update checkauth.php

--- checkauth.php
+++ checkauth.php
@@ -19,7 +19,7 @@ if(!empty($_POST["EmployeeID"]) && !empty($_POST["Password"])) {
             header('Location: login.php');
      }
    
-if("$password" == "$row[0]") {
+if($password == $row[0]) {
   session_start();
   $_SESSION["authenticated"] = 'true';
   header('Location: dashboard.php');
