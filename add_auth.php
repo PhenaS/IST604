caeb3ac457603effe3ff0c957fece4a0b32dc317
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-11 15:21:40 -0400

    Update add_auth.php

--- add_auth.php
+++ add_auth.php
@@ -6,5 +6,6 @@ try{
     $sql = ("INSERT INTO individual (first_name, last_name, birthdate) VALUES ('" . $_POST["FirstName"] . "','" . $_POST["LastName"] . "', '" . $_POST["bday"] . "')"); 
     $stmt = $conn->query("$sql");
     $conn = NULL;
+    echo "Succesfully Added!";
     }catch(Exception $e){die(print_r($e));}    
 ?>
