17f735dea668c8ecde0f06d2630579d38c8e6fbe
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-11 15:18:44 -0400

    Update add_auth.php

--- add_auth.php
+++ add_auth.php
@@ -5,7 +5,6 @@ include 'config.php';
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = ("INSERT INTO individual (first_name, last_name, birthdate) VALUES ('" . $_POST["FirstName"] . "','" . $_POST["LastName"] . "', '" . $_POST["bday"] . "')"); 
     $stmt = $conn->query("$sql");
-    $row = $stmt->fetch();
     $conn = NULL;
     }catch(Exception $e){die(print_r($e));}    
 ?>
