6a4bbd5b82643e1e3a8726970b42a6630931d304
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-11 15:15:58 -0400

    Update add_employee.php

--- add_employee.php
+++ add_employee.php
@@ -19,7 +19,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
   <div class="w3-col s4">
     <h4>Solutions at Work Inc</h4>
     <p>Add a Consumer</p>
-      <form id="Add" name="Add" action="add_auth.php" method="post">
+      <form id="Add" name="Add" form action="add_auth.php" method="post">
         <p><input class="w3-input w3-border" type="text" placeholder="First Name" name="FirstName" required></p>
         <p><input class="w3-input w3-border" type="text" placeholder="Last name" name="LastName" required></p>
           <p>Birthdate<input class="w3-input w3-border" type="date" name="bday"></p>
