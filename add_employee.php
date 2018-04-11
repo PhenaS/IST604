05eb7558c46dde78217b1bfc263c8f9d38978340
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-11 15:13:41 -0400

    Update add_employee.php

--- add_employee.php
+++ add_employee.php
@@ -28,17 +28,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
      
 </body>
       
-<?php
-include 'config.php';      
-if(!empty($_POST["FirstName"]) && !empty($_POST["LastName"]) && !empty($_POST["bday"])) {try{
-    $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = ("INSERT INTO individual (first_name, last_name, birthdate) VALUES ('" . $_POST["FirstName"] . "','" . $_POST["LastName"] . "', '" . $_POST["bday"] . "')"); 
-    $stmt = $conn->query("$sql");
-    $row = $stmt->fetch();
-    $conn = NULL;
-    }catch(Exception $e){die(print_r($e));}    
-?>
+
 </html>
 
 
