8d76cac7e6441b68773b8921b5507aaa31b3bd2a
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-11 15:17:08 -0400

    Update add_auth.php

--- add_auth.php
+++ add_auth.php
@@ -1,6 +1,6 @@
 <?php
 include 'config.php';      
-if(!empty($_POST["FirstName"]) && !empty($_POST["LastName"]) && !empty($_POST["bday"])) {try{
+{try{
     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = ("INSERT INTO individual (first_name, last_name, birthdate) VALUES ('" . $_POST["FirstName"] . "','" . $_POST["LastName"] . "', '" . $_POST["bday"] . "')"); 
