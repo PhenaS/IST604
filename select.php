ea98514dd675ba0aa65a66dd5bc252dde17922c4
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-13 14:16:51 -0400

    Update select.php

--- select.php
+++ select.php
@@ -4,16 +4,9 @@ include 'config.php';
 try{
     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $result = $db->prepare("SELECT first_name, last_name FROM individual");
-    $result->execute();
     
-    while ($row = $result->fetch(PDO::FETCH_ASSOC))
-    {
-    $title = $row['first_name'];
-    $body = $row['last_name'];
-    }
-
-    echo $title;
-    echo $body;
-
+    $tickets = $db->query('SELECT * FROM individual')->fetchAll();
+    foreach($tickets as $ticket) {
+    echo $ticket['first_name'];
+}
 ?>
