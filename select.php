5db67871d61234b3174265139dac307176dce639
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-13 14:01:53 -0400

    Update select.php

--- select.php
+++ select.php
@@ -1,10 +1,5 @@
-
-      
 <?php
 include 'config.php';    
-if(empty($_POST["FirstName"]) or empty($_POST["LastName"]) or empty($_POST["bday"])){
-    header('Location: add_employee.php');
-}else{
 
 try{
     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
@@ -15,5 +10,5 @@ try{
     while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
     echo $row['first_name'].", ".$row['last_name']."<br />";
     }catch(Exception $e){die(print_r($e));}    
-}
+
 ?>
