6b80250832ca6d27135175b7e3b28126215ca208
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-13 14:30:34 -0400

    Update select.php

--- select.php
+++ select.php
@@ -5,7 +5,8 @@ try{
     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $row = $db->query('SELECT first_name FROM individual')->fetchAll();
-    foreach($row as $ticket) {
+    foreach($row as $ticket){
     echo $ticket['first_name'];
+    }
 }catch(Exception $e){die(print_r($e));}
 ?>
