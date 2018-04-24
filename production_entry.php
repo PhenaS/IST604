1584fad9c426e30ef7ef11b43873af00a5c0d8c8
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-24 13:37:59 -0400

    Add files via upload

--- production_entry.php
+++ production_entry.php
@@ -13,16 +13,17 @@
   <body>
     <h1>SAW Proudction Sheet Entry</h1>
 
-    <form>
+    <form class="form-inline">
     <div class="form-row align-items-center">
     <div class="col-auto">
       <label for="inlineFormCustomSelect">Individual</label>
       <div class="input-group mb-2">
+      <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
       <select class="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
         <option selected>Choose...</option>
-        <option value="1">Charlie Kelly</option>
-        <option value="2">Phena Senediak</option>
-        <option value="3">Abby Stein</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
       </select>
 
     </div>
