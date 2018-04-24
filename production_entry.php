57f107ba90adba8da43185bc4285b959879cd21f
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-24 19:33:10 -0400

    Add files via upload

--- production_entry.php
+++ production_entry.php
@@ -15,18 +15,27 @@
   <h1>SAW Proudction Sheet Entry</h1>
 
 <form class="form-inline" action="/action_page.php">
+  
+<select class="form-control form-control-lg">
+  <option>Large select</option>
+</select>
+
   <div class="form-group">
     <label for="email">Email address:</label>
     <input type="email" class="form-control" id="email">
   </div>
+  
   <div class="form-group">
     <label for="pwd">Password:</label>
     <input type="password" class="form-control" id="pwd">
   </div>
+  
   <div class="checkbox">
     <label><input type="checkbox"> Remember me</label>
   </div>
+  
   <button type="submit" class="btn btn-default">Submit</button>
+
 </form>
 
 </html>
