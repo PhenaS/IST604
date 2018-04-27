2ff1ae4017d422246cf1d5a7f66911b5b7fc5a1c
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-27 10:31:11 -0400

    Add files via upload

--- prod_entry_auth.php
+++ prod_entry_auth.php
@@ -1,8 +1,22 @@
 <?php
 $timein = $_POST["TimeIn"];
 $timeout = $_POST["TimeOut"];
-
+$indid = $_POST["Individual"];
+$jobid = $_POST["job"];
+$opid = $_POST["op"];
+$qty = $_POST["piece_qty"];
+$workdate = $_POST["work_date"];
 $workingHours = (strtotime($timeout) - strtotime($timein)) / 3600;
 
 echo $workingHours;
+echo $timein;
+echo $timeout;
+echo $indid;
+echo $jobid;
+echo $opid;
+echo $qty;
+echo $workdate;
+
+
+
 ?>
