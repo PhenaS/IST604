055c61fed9c93bced74b7a4129923077d5fe790f
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-27 10:59:48 -0400

    Add files via upload

--- prod_entry_auth.php
+++ prod_entry_auth.php
@@ -8,63 +8,7 @@ $qty = $_POST["piece_qty"];
 $workdate = $_POST["work_date"];
 
 $workingHours = (strtotime($timeout) - strtotime($timein)) / 3600;
+echo $_SESSION["employee_id"];
 
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-table {
-    border-collapse: collapse;
-    width: 100%;
-}
-
-th, td {
-    text-align: left;
-    padding: 8px;
-}
-
-tr:nth-child(even){background-color: #f2f2f2}
-
-th {
-    background-color: #4CAF50;
-    color: white;
-}
-</style>
-</head>
-<body>
-
-<h2>Colored Table Header</h2>
-
-<table>
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    <th>Savings</th>
-  </tr>
-  <tr>
-    <td>Peter</td>
-    <td>Griffin</td>
-    <td>$100</td>
-  </tr>
-  <tr>
-    <td>Lois</td>
-    <td>Griffin</td>
-    <td>$150</td>
-  </tr>
-  <tr>
-    <td>Joe</td>
-    <td>Swanson</td>
-    <td>$300</td>
-  </tr>
-  <tr>
-    <td>Cleveland</td>
-    <td>Brown</td>
-    <td>$250</td>
-</tr>
-</table>
-
-</body>
-</html>
