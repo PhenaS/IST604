1f7d2a141cd71ff855a37e436f70e953c37cdded
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-04-27 17:25:38 -0400

    Add files via upload

--- ReviewProd.php
+++ ReviewProd.php
@@ -1,9 +1,6 @@
-<!doctype html>
-
-<html lang="en">
 
 <head>
-<div id="demo">
+
 <style>
 
 /* -- import Roboto Font ---------------------------- */
@@ -729,7 +726,11 @@ body {
 }
 
 </style>
+
 </head>
+
+
+<div id="demo">
   <h1>Material Design Responsive Table</h1>
   <h2>Table of my other Material Design works (list was updated 08.2015)</h2>
   
@@ -880,63 +881,9 @@ body {
   </p>  
   </div>
   <div style="width: 45%; display: inline-block; vertical-align: top; margin-left: 30px;">
-    <h2>Description</h2>$(document).ready(function() {
-
-var table = $('#table');
-
-// Table bordered
-$('#table-bordered').change(function() {
-    var value = $( this ).val();
-    table.removeClass('table-bordered').addClass(value);
-});
-
-// Table striped
-$('#table-striped').change(function() {
-    var value = $( this ).val();
-    table.removeClass('table-striped').addClass(value);
-});
-
-// Table hover
-$('#table-hover').change(function() {
-    var value = $( this ).val();
-    table.removeClass('table-hover').addClass(value);
-});
-
-// Table color
-$('#table-color').change(function() {
-    var value = $(this).val();
-    table.removeClass(/^table-mc-/).addClass(value);
-});
-});
-
-// jQuery’s hasClass and removeClass on steroids
-// by Nikita Vasilyev
-// https://github.com/NV/jquery-regexp-classes
-(function(removeClass) {
-
-jQuery.fn.removeClass = function( value ) {
-    if ( value && typeof value.test === "function" ) {
-        for ( var i = 0, l = this.length; i < l; i++ ) {
-            var elem = this[i];
-            if ( elem.nodeType === 1 && elem.className ) {
-                var classNames = elem.className.split( /\s+/ );
-
-                for ( var n = classNames.length; n--; ) {
-                    if ( value.test(classNames[n]) ) {
-                        classNames.splice(n, 1);
-                    }
-                }
-                elem.className = jQuery.trim( classNames.join(" ") );
-            }
-        }
-    } else {
-        removeClass.call(this, value);
-    }
-    return this;
-}
-
-})(jQuery.fn.removeClass);
+    <h2>Description</h2>
     <p>Tested on Win8.1 with browsers: Chrome 37, Firefox 32, Opera 25, IE 11, Safari 5.1.7</p>
     <p>You can use this table in Bootstrap (v3) projects. Material Design Responsive Table CSS-style will override basic bootstrap style.</p>
     <p class="mdt-subhead-2"><strong>Donate:</strong> You can support me via <a class="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=s%2ekupletsky%40gmail%2ecom&amp;lc=US&amp;item_name=Material%20Design%20Responsive%20Table&amp;currency_code=USD&amp;bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted">PayPal</a>, <a class="webmoney" href="https://funding.webmoney.ru/material-design-iconic-font/donate">WebMoney</a> or <a class="gratipay" href="http://gratipay.com/zavoloklom/" target="_blank">Gratipay</a></p>
-  <
+  </div>
+</div>
