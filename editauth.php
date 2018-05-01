814952ceee6100ba22f2fcb45424cbb600b1086d
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-01 11:34:53 -0400

    Add files via upload

--- editauth.php
+++ editauth.php
@@ -9,6 +9,7 @@ try{
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = ("UPDATE piece_production_sheet SET hours_worked = " . $_POST["Hours"] . ", individual_id = " . $_POST["IndID"] . ", op_id = " . $_POST["opnum"] . ", job_id = " . $_POST["jobnum"] . ", pieces_produced = " . $_POST["qty"] . " WHERE line_id = " . $_SESSION["line"] . ""); 
     $stmt = $conn->query("$sql");
+    $stmt->nextRowset();
     $row = $stmt->fetch();
     $conn = NULL;
     header("Location: Reviewprod.php");
