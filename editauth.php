5d9159e52129d393806997815695175bb52a94a7
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-01 11:26:16 -0400

    Add files via upload

--- editauth.php
+++ editauth.php
@@ -1,11 +1,13 @@
 <?php 
-   
+
+session_start();
+
 try{
     include 'config.php';
     
     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = ("UPDATE piece_production_sheet SET hours_worked = " . $_POST["Hours"] . ", individual_id = " . $_POST["IndID"] . ", op_id = " . $_POST["opnum"] . ", job_id = " . $_POST["jobnum"] . ", emp_id = " . $_POST["EmployeeID"] . ", pieces_produced = " . $_POST["qty"] . " WHERE line_id = " . $_POST["line"] . ""); 
+    $sql = ("UPDATE piece_production_sheet SET hours_worked = " . $_POST["Hours"] . ", individual_id = " . $_POST["IndID"] . ", op_id = " . $_POST["opnum"] . ", job_id = " . $_POST["jobnum"] . ", emp_id = " . $_POST["EmployeeID"] . ", pieces_produced = " . $_POST["qty"] . " WHERE line_id = " . $_SESSION["line"] . ""); 
     $stmt = $conn->query("$sql");
     $row = $stmt->fetch();
     $conn = NULL;
