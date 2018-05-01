beee527d49a370253d10448b5f761d9ace42c772
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-01 11:20:22 -0400

    Add files via upload

--- editauth.php
+++ editauth.php
@@ -5,7 +5,7 @@ try{
     
     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = ("UPDATE piece_production_sheet SET hours_worked = " . $_POST["Hours"] . " , individual_id = " . $_POST["IndID"] . ", op_id = " . $_POST["opnum"] . ", job_id = " . $_POST["jobnum"] . ", individual_id = " . $_POST["EmployeeID"] . ", pieces_produced = " . $_POST["qty"] . " WHERE = " . $_POST["line"] . ""); 
+    $sql = ("UPDATE piece_production_sheet SET hours_worked = " . $_POST["Hours"] . ", individual_id = " . $_POST["IndID"] . ", op_id = " . $_POST["opnum"] . ", job_id = " . $_POST["jobnum"] . ", individual_id = " . $_POST["EmployeeID"] . ", pieces_produced = " . $_POST["qty"] . " WHERE = " . $_POST["line"] . ""); 
     $stmt = $conn->query("$sql");
     $row = $stmt->fetch();
     $conn = NULL;
