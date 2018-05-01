fa8d253f54df84a400b77ab12eb4d7ca1329420e
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-01 19:04:51 -0400

    Add files via upload

--- test.php
+++ test.php
@@ -1,10 +1,5 @@
 <?php
 session_start();
 
-echo $_SESSION['table'][0][0];
-echo $_SESSION['table'][0][1];
-echo $_SESSION['table'][1][0];
-echo $_SESSION['table'][1][1];
-]
-
+echo $_SESSION['table']
 ?>
