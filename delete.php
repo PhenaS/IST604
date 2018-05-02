d1e4b3ae9cbd9ce74cdea3c91fa23679247cca41
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-02 12:47:33 -0400

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,7 +1,20 @@
 <?php
-
 session_start();
+if($_SESSION["authenticated"] !== 'true'){
+   header('Location: login.php');   
+   }
 
-echo $_SESSION['linedel'];
-
+include 'config.php';    
+ 
+try{
+     $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $sql = ("DELETE FROM piece_production_sheet WHERE line_id =  " . $_SESSION['line'] . ""); 
+     $stmt = $conn->query("$sql");
+     $conn = NULL;
+     echo "Succesfully Deleted!";
+     header("refresh:1;url=production_entry.php");
+     }catch(Exception $e){die(print_r($e));}    
+ 
 ?>
+ 
