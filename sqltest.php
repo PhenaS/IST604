299c369c051bb975fa8b77a29c456217810d260f
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-07 21:44:37 -0400

    Add files via upload

--- sqltest.php
+++ sqltest.php
@@ -1,20 +1,6 @@
 <?php
-$host = 'phena.databae.windows.net';
-$username = 'senediak@phena';
-$password = 'ste11PHEN**';
-$db_name = 'phena';
 
-//Establishes the connection
-$conn = mysqli_init();
-sqlsrv_connect($conn, $host, $username, $password, $db_name);
+print_r($_SESSION['data']);
 
-//Run the Select query
-printf("Reading data from table: \n");
-$res = sql_query($conn, 'SELECT * FROM staff');
-while ($row = sqlsrv_fetch_array($res)) {
-var_dump($row);
-}
 
-//Close the connection
-sqlsrv_close($conn);
 ?>
