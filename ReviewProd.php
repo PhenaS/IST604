154a19f4ac104a7886820bb2caac027855d6c84c
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-07 21:27:56 -0400

    Add files via upload

--- ReviewProd.php
+++ ReviewProd.php
@@ -13,7 +13,7 @@
         include 'config.php';
         $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = ("SELECT line_id, time_stamp, entry_date, emp_id, individual_id, op_id, hours_worked, job_id, pieces_produced FROM piece_production_sheet WHERE  entry_date >= 'CURRENT_TIMESTAMP' AND emp_id = " . $empid . ""); 
+        $sql = ("SELECT line_id, time_stamp, entry_date, emp_id, individual_id, op_id, hours_worked, job_id, pieces_produced FROM piece_production_sheet WHERE  entry_date >= CURRENT_TIMESTAMP AND emp_id = " . $empid . ""); 
         $stmt = $conn->query("$sql");
         }catch(Exception $e){die(print_r($e));}
 
