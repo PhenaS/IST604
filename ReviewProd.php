7d8b07c4f6cd14f2b7ced88382fe80c8f46c6bef
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-07 21:27:12 -0400

    Add files via upload

--- ReviewProd.php
+++ ReviewProd.php
@@ -15,7 +15,7 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = ("SELECT line_id, time_stamp, entry_date, emp_id, individual_id, op_id, hours_worked, job_id, pieces_produced FROM piece_production_sheet WHERE  entry_date >= 'CURRENT_TIMESTAMP' AND emp_id = " . $empid . ""); 
         $stmt = $conn->query("$sql");
-        }catch(Exception $e){die(print_r($e));} date string in the format of 2013-12-29
+        }catch(Exception $e){die(print_r($e));}
 
 
  ?>
