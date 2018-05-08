ef8fcb7b5fbc94c24aa050884524485244919be3
PhenaS <37418009+PhenaS@users.noreply.github.com>
2018-05-07 21:20:27 -0400

    Add files via upload

--- ReviewProd.php
+++ ReviewProd.php
@@ -7,14 +7,14 @@
     if($_SESSION["authenticated"] !== 'true'){
      header('Location: login.php');   
     }
-    $empid = $_SESSION['Empid'];
+    $empid = $_SESSION['Empid'];$now = strtotime("now");
 
     try{
         include 'config.php';
-        $now = strtotime("now");
         $conn = new PDO ("sqlsrv:Server = $host; Database = $db", $user, $pwd);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = ("SELECT line_id, time_stamp, entry_date, emp_id, individual_id, op_id, hours_worked, job_id, pieces_produced FROM piece_production_sheet WHERE  entry_date >= "' . $now . '" AND emp_id = " . $empid . ""); 
+        $now = strtotime("now");
+        $sql = ("SELECT line_id, time_stamp, entry_date, emp_id, individual_id, op_id, hours_worked, job_id, pieces_produced FROM piece_production_sheet WHERE  entry_date >= '" . $now . "' AND emp_id = " . $empid . ""); 
         $stmt = $conn->query("$sql");
         }catch(Exception $e){die(print_r($e));}
  ?>
